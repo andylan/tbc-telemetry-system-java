91dfb8a83b41d4fd84edaa583fe95dafcdb7db08
Ben Wu <wubin28@gmail.com>
2014-06-13 18:43:48 +0800

    Working on TODO: it can use a telemetry client to send a diagnostic message and receive a diagnostic info. Wrote an assertion for the test of this TODO.

--- ts/src/test/java/kata/ts/TelemetryDiagnosticControlsTest.java
+++ ts/src/test/java/kata/ts/TelemetryDiagnosticControlsTest.java
@@ -8,14 +8,16 @@ import static org.junit.Assert.assertEquals;
  * Created by benwu on 14-6-10.
  */
 public class TelemetryDiagnosticControlsTest {
-    @Test
-    public void a_trivial_test(){
-        assertEquals(5, 2 + 3);
-    }
 
     // TODO-acceptance-test: retry to connect to the server for maximum 3 times
     // TODO-acceptance-test: throw an exception if it cannot connect to the server for more than 3 times
-    // TODO-acceptance-test: it can use a telemetry client to send a diagnostic message and receive a diagnostic info
+
+    // TODO-acceptance-test-working-on: it can use a telemetry client to send a diagnostic message and receive a diagnostic info
+    @Test
+    public void it_can_use_a_telemetry_client_to_send_a_diagnostic_message_and_receive_a_diagnostic_info() {
+        // Assert
+        assertEquals("", telemetryDiagnosticControls.getDiagnosticInfo());
+    }
 
     // TODO-new-feature: Retry to connect to the server for maximum 5 times
 }
