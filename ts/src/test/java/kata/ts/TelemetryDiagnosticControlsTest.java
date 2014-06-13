22b91c7d8812f14e0d47cae37fd17dae061f159c
Ben Wu <wubin28@gmail.com>
2014-06-13 11:09:33 +0800

    Added 3 acceptance test TODOs.

--- ts/src/test/java/kata/ts/TelemetryDiagnosticControlsTest.java
+++ ts/src/test/java/kata/ts/TelemetryDiagnosticControlsTest.java
@@ -13,5 +13,9 @@ public class TelemetryDiagnosticControlsTest {
         assertEquals(5, 2 + 3);
     }
 
+    // TODO-acceptance-test: retry to connect to the server for maximum 3 times
+    // TODO-acceptance-test: throw an exception if it cannot connect to the server for more than 3 times
+    // TODO-acceptance-test: it can use a telemetry client to send a diagnostic message and receive a diagnostic info
+
     // TODO-new-feature: Retry to connect to the server for maximum 5 times
 }
